7eab930f26fb37df894fa671f73a79fd8e8836bb
WARsageR <warsager109@gmail.com>
2018-07-06 02:12:47 +0300

    Добавлен Lua. Signed-off-by: WARsageR <warsager109@gmail.com>

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -1,10 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include "lua.hpp"
 
 int main()
 {
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
 	sf::RenderWindow window(sf::VideoMode(200, 200), "Lesson 2. kychka-pc.ru");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Red);
+
+	luaL_loadfile(L, "testHello.lua");
+	lua_call(L, 0, 0);
 
 	while (window.isOpen())
 	{
@@ -19,6 +26,10 @@ int main()
 		window.draw(shape);
 		window.display();
 	}
+
+
+
+
 
 	return 0;
 }
